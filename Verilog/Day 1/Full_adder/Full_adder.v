397c6f153808bad954173a24060d8cc9e0e034ae
Sriniketh Sekar <148874325+SrinikethSekar@users.noreply.github.com>
2024-12-31 16:05:03 +0530

    Update Full_adder.v

--- Verilog/Day 1/Full_adder/Full_adder.v
+++ Verilog/Day 1/Full_adder/Full_adder.v
@@ -1,17 +1,17 @@
 `timescale 1ns / 1ps
 
 module Full_adder(
-input a,b,
-input c_in,
-output sum,carry
+input a,b,            //input of the full adder
+input c_in,            //carry is the main operations, because we are using two half adders
+output sum,carry        //output of the full adder
     );
 wire x,y,z;
 
-xor a1(x,a,b);
+xor a1(x,a,b);            //these gate is used for the first half adder circuit
 xor a2(sum,x,c_in);
 
 and b1(y,x,c_in);
-and b2(z,a,b);
+and b2(z,a,b);            //these gates are used for the second half adder circuit
 or c1(carry,y,z);
 
 endmodule
