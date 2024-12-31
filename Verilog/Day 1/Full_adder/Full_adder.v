40a5344bcf7e7ce5d94aa57de850b6d77898e63d
Sriniketh Sekar <148874325+SrinikethSekar@users.noreply.github.com>
2024-12-31 15:39:08 +0530

    Update Full_adder.v

--- Verilog/Day 1/Full_adder/Full_adder.v
+++ Verilog/Day 1/Full_adder/Full_adder.v
@@ -1,1 +1,18 @@
-module
+`timescale 1ns / 1ps
+
+module Full_adder(
+input a,b,
+input c_in,
+output sum,carry
+    );
+wire x,y,z;
+
+xor a1(x,a,b);
+xor a2(sum,x,c_in);
+
+and b1(y,x,c_in);
+and b2(z,a,b);
+or c1(carry,y,z);
+
+endmodule
+
