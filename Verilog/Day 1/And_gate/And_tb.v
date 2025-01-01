11d4b9c83f99c3cbb28ec1e92231c76a8dda8c28
Sriniketh Sekar <148874325+SrinikethSekar@users.noreply.github.com>
2025-01-01 09:15:24 +0530

    Create And_tb.v

---
+++ Verilog/Day 1/And_gate/And_tb.v
@@ -0,0 +1,14 @@
+module And_tb();
+  reg a,b;
+  wire c;
+
+  And and1(.x(x),.y(y),.z(z));
+
+  initial 
+    begin
+      a=0; b=0; #10;
+      a=0; b=1; #10;
+      a=1; b=0; #10;
+      a=1; b=1; #10;
+    end
+endmodule
