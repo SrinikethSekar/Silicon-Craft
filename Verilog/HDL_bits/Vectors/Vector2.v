2d8fe70948452e8d65290d6d3fd9dc1ee8c6125d
Sriniketh Sekar <148874325+SrinikethSekar@users.noreply.github.com>
2025-01-07 21:03:43 +0530

    Update Vector2.v

--- Verilog/HDL_bits/Vectors/Vector2.v
+++ Verilog/HDL_bits/Vectors/Vector2.v
@@ -9,3 +9,7 @@ module top_module (
 	assign out[ 7: 0] = in[31:24];	
 	
 endmodule
+
+//Based on the given parameters, the bit is divided into four 8 parts and that need to be printed in reverse order.
+
+//First 8 bit need to be reverse in the last and last 8 bit need to be reverse to the first
