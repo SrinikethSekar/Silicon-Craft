4e0b4c76258d1eba59a75fb34780b7b173779e51
Sriniketh Sekar <148874325+SrinikethSekar@users.noreply.github.com>
2025-01-07 21:05:13 +0530

    Update gates4.v

--- Verilog/HDL_bits/Vectors/gates4.v
+++ Verilog/HDL_bits/Vectors/gates4.v
@@ -4,8 +4,8 @@ module top_module(
     output out_or,
     output out_xor
 );
-    assign out_or =(in[0]|in[1]|in[2]|in[3]);
-    assign out_and =(in[0]&in[1]&in[2]&in[3]);
-    assign out_xor =(in[0]^in[1]^in[2]^in[3]);
+    assign out_or =(in[0]|in[1]|in[2]|in[3]); //or Operator 
+    assign out_and =(in[0]&in[1]&in[2]&in[3]);//and Operator 
+    assign out_xor =(in[0]^in[1]^in[2]^in[3]);//xor operator 
 
 endmodule
