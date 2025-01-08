95896dc8931d838de23a5393e6fa9ad37acc048f
Sriniketh Sekar <148874325+SrinikethSekar@users.noreply.github.com>
2025-01-08 20:24:46 +0530

    Update module_cseladd.v

--- Verilog/HDL_bits/Module/module_cseladd.v
+++ Verilog/HDL_bits/Module/module_cseladd.v
@@ -5,9 +5,9 @@ module top_module(
 );
     wire [15:0]sum1,sum2,sum3;
     wire cout;
-    add16 inst1(a[15:0],b[15:0],1'b0,sum1[15:0],cout);
-    add16 inst2(a[31:16],b[31:16],1'b0,sum2[15:0],0);
-    add16 inst3(a[31:16],b[31:16],1'b1,sum3[15:0],0);
+    add16 inst1(a[15:0],b[15:0],1'b0,sum1[15:0],cout);  //first adder 
+    add16 inst2(a[31:16],b[31:16],1'b0,sum2[15:0],0); //second adder
+    add16 inst3(a[31:16],b[31:16],1'b1,sum3[15:0],0);//third adder
     always@(*)
         begin
             case(cout)
