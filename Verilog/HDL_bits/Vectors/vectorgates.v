094c554a371fc8e01ca088fe6c45ead9045d55bf
Sriniketh Sekar <148874325+SrinikethSekar@users.noreply.github.com>
2025-01-08 20:02:20 +0530

    Update vectorgates.v

--- Verilog/HDL_bits/Vectors/vectorgates.v
+++ Verilog/HDL_bits/Vectors/vectorgates.v
@@ -5,9 +5,9 @@ module top_module(
     output out_or_logical,
     output [5:0] out_not
 );
-    assign out_or_bitwise= a|b;
-    assign out_or_logical= a||b;
-    assign out_not[5:3]=~(b);
+    assign out_or_bitwise= a|b;   //bitwise or Operator 
+    assign out_or_logical= a||b; //logical or operator 
+    assign out_not[5:3]=~(b); //assign the output to the input by inverted
     assign out_not[2:0]=~(a);
 
 endmodule
