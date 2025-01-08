f185ef2be4e4a38993751120bc4f834c6ee5a3f9
Sriniketh Sekar <148874325+SrinikethSekar@users.noreply.github.com>
2025-01-08 20:07:28 +0530

    Update module.v

--- Verilog/HDL_bits/Module/module.v
+++ Verilog/HDL_bits/Module/module.v
@@ -1,4 +1,3 @@
 module top_module ( input a, input b, output out );
-    wire wc,wa,wb;
     mod_a instance2 ( . out(out), .in1(a), .in2(b) );
-endmodule
+endmodule //instantiate the input to the given module
