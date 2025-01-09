a4bb4c7cf025c91ad6a9bd5c93d7782659069688
Sriniketh Sekar <148874325+SrinikethSekar@users.noreply.github.com>
2025-01-09 20:42:42 +0530

    Update module_shift.v

--- Verilog/HDL_bits/Module/module_shift.v
+++ Verilog/HDL_bits/Module/module_shift.v
@@ -1,6 +1,6 @@
 module top_module ( input clk, input d, output q );
     wire a,b;
-    my_dff d1(clk,d,a);
-    my_dff d2(clk,a,b);
-    my_dff d3(clk,b,q);
+    my_dff d1(clk,d,a);  //first shift
+    my_dff d2(clk,a,b);  //second shift 
+    my_dff d3(clk,b,q);  //third shift
 endmodule
