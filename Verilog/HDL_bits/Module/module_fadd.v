ca01b7f631f2d86cbed2a9f8956c98c0993147db
Sriniketh Sekar <148874325+SrinikethSekar@users.noreply.github.com>
2025-01-09 20:38:36 +0530

    Update module_fadd.v

--- Verilog/HDL_bits/Module/module_fadd.v
+++ Verilog/HDL_bits/Module/module_fadd.v
@@ -3,7 +3,7 @@ module top_module (
     input [31:0] b,
     output [31:0] sum
 );
-    wire cout1;
+    wire cout1; 
     wire [15:0] sum1,sum2;
     add16 (a[15:0],b[15:0],0,sum1[15:0],cout1);
     add16 (a[31:16],b[31:16],cout1,sum2[15:0],0);
@@ -12,6 +12,6 @@ module top_module (
 endmodule
 
 module add1 ( input a, input b, input cin,   output sum, output cout );
-    assign {cout,sum}=a+b+cin;
+    assign {cout,sum}=a+b+cin; // first module is called here for the output
     
 endmodule
