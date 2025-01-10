41620d6a17c39e4cb4cdfe9e501187959c35c411
Sriniketh Sekar <148874325+SrinikethSekar@users.noreply.github.com>
2025-01-10 23:44:02 +0530

    Update always_block2.v

--- Verilog/HDL_bits/Procedures/always_block2.v
+++ Verilog/HDL_bits/Procedures/always_block2.v
@@ -6,9 +6,10 @@ module top_module(
     output reg out_always_comb,
     output reg out_always_ff   );
     
-    assign out_assign=a^b;
-    always@(*) out_always_comb = a^b;
+    assign out_assign=a^b;  //create a or gate using assignment statement 
+    always@(*) out_always_comb = a^b;//same using always statement 
     always@(posedge clk)
         out_always_ff<=a^b;
 
 endmodule
+ 
