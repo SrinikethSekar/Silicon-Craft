cd5c928f9a0ba73bddc1e421dd67161c6226f420
Sriniketh Sekar <148874325+SrinikethSekar@users.noreply.github.com>
2025-01-13 22:33:43 +0530

    Update popcount255.v

--- Verilog/HDL_bits/Verilog_features/popcount255.v
+++ Verilog/HDL_bits/Verilog_features/popcount255.v
@@ -1,4 +1,10 @@
-integer i;
+
+module top_module( 
+    input [254:0] in,
+    output [7:0] out );
+
+    reg [7:0] count;
+    integer i;
     always @(*) begin
         count = 8'b0;  // Reset count
         for (i = 0; i < 255; i = i + 1) begin
