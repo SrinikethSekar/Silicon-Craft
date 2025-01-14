b0c859a4d4538c6f3cc800c951e96c6f062be7e9
Sriniketh Sekar <148874325+SrinikethSekar@users.noreply.github.com>
2025-01-14 23:38:01 +0530

    Update gates100.v

--- Verilog/HDL_bits/Verilog_features/gates100.v
+++ Verilog/HDL_bits/Verilog_features/gates100.v
@@ -4,7 +4,7 @@ module top_module(
     output out_or,
     output out_xor 
 );
-    assign out_and =(&in[99:0]);
+    assign out_and =(&in[99:0]); //or gate for 99 input 
     assign out_or =(|in[99:0]);
     assign out_xor =(^in[99:0]);
     
