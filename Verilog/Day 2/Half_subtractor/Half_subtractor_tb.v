041134d4b6257f85abc923354eba1399500814db
Sriniketh Sekar <148874325+SrinikethSekar@users.noreply.github.com>
2025-01-23 19:52:34 +0530

    Update Half_subtractor_tb.v

--- Verilog/Day 2/Half_subtractor/Half_subtractor_tb.v
+++ Verilog/Day 2/Half_subtractor/Half_subtractor_tb.v
@@ -3,11 +3,11 @@ reg a,b;
 wire d,b0;
     
     Half_subtractor uut(.a(a),.b(b),.d(d),.b0(b0));
- /*   
 initial begin
 $dumpfile("waves.vcd");
 $dumpfile();
-end*/
+end
+
 initial begin
 $monitor("Time=%0t | a=%b, b=%b | d=%b, b0=%b", $time, a, b, d, b0); 
 a=0; b=0; #10;
