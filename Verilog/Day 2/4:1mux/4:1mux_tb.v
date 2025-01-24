8e52f8bea5ee1ed1b56a96b146754202da4385e9
Sriniketh Sekar <148874325+SrinikethSekar@users.noreply.github.com>
2025-01-24 20:45:21 +0530

    Update 4:1mux_tb.v

--- Verilog/Day 2/4:1mux/4:1mux_tb.v
+++ Verilog/Day 2/4:1mux/4:1mux_tb.v
@@ -14,9 +14,9 @@ integer i,j;
 initial 
 begin
 	$monitor(" data_in=%b sel_in=%b  y_out=%b",data_in,sel_in,y_out);
-data_in = 4'b1010; sel_in = 2'b00; #10;
-data_in = 4'b1010; sel_in = 2'b01; #10;
-data_in = 4'b1010; sel_in = 2'b10; #10;
+data_in = 4'b1010; sel_in = 2'b00; #10;//0th bit is called in the 10101
+data_in = 4'b1010; sel_in = 2'b01; #10; 
+data_in = 4'b1010; sel_in = 2'b10; #10;//2nd bit is called in the 1010
 data_in = 4'b1010; sel_in = 2'b11; #10;
 	
 	
