6cc0fac25160865d39ece5af67e475dd2b4e5891
Sriniketh Sekar <148874325+SrinikethSekar@users.noreply.github.com>
2025-01-25 23:56:41 +0530

    Update Full_subtractor_tb.v

--- Verilog/Day 2/Full_subtractor/Full_subtractor_tb.v
+++ Verilog/Day 2/Full_subtractor/Full_subtractor_tb.v
@@ -11,7 +11,7 @@ $dumpfile("waves.vcd");
 $dumpvars(0,Full_subtractor_tb);
 end
 initial begin
-  $monitor("Time=%0t | a=%b, b=%b c=%b | diff=%b, borrow=%b", $time, a, b,c, diff, borrow); //below table follows the logic of subtractor
+  $monitor("Time=%0t | a=%b, b=%b c=%b | diff=%b, borrow=%b", $time, a, b,c, diff, borrow); //below table follows the logic of subtractor truth table 
 a=0; b=0; c=0; #10;
 a=0; b=0; c=1; #10;
 a=0; b=1; c=0; #10;
