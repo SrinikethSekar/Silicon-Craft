f3220d4c61a314e54ed37295a83d379772e064bc
Sriniketh Sekar <148874325+SrinikethSekar@users.noreply.github.com>
2025-01-25 23:58:24 +0530

    Update Ripple_carry_adder.v

--- Verilog/Day 3/Ripple_carry_adder/Ripple_carry_adder.v
+++ Verilog/Day 3/Ripple_carry_adder/Ripple_carry_adder.v
@@ -9,7 +9,7 @@ wire c1,c2,c3;
 
 fa aa1(.a(a0),.b(b0),.cin(cin),.sum(s0),.carry(c1));//first carry to 2nd
 fa aa2(.a(a1),.b(b1),.cin(c1),.sum(s1),.carry(c2));//second carry to third
-fa aa3(.a(a2),.b(b2),.cin(c2),.sum(s2),.carry(c3));
+fa aa3(.a(a2),.b(b2),.cin(c2),.sum(s2),.carry(c3));//third carry to last one
 fa aa4(.a(a3),.b(b3),.cin(c3),.sum(s3),.carry(c0));
 
 endmodule
