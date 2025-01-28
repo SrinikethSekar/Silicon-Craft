9acf060d29e22856ee8c26e5f7b9081d0d0a27e4
Sriniketh Sekar <148874325+SrinikethSekar@users.noreply.github.com>
2025-01-28 12:13:43 +0530

    Create JK_flip_flop.v

---
+++ Verilog/Day 5/JK_flip_flop/JK_flip_flop.v
@@ -0,0 +1,22 @@
+module sr_ff(
+  input j,k,
+  input clk,rst,
+  output reg q,
+  output qb);
+  
+  always@(posedge clk)begin
+    if(rst)
+      q<=0;
+    else
+      case({j,k})
+        2'b00:q<=q;
+        2'b01:q<=1'b0;
+        2'b10:q<=1'b1;
+        2'b11:q<=~q;
+      endcase
+  end
+  assign qb=~q;
+endmodule
+
+
+
