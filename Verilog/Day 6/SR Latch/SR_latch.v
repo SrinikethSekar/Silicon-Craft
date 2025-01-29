ded8adb8f551922dc9b5237befdab1c6946b1a1a
Sriniketh Sekar <148874325+SrinikethSekar@users.noreply.github.com>
2025-01-29 17:09:47 +0530

    Create SR_latch.v

---
+++ Verilog/Day 6/SR Latch/SR_latch.v
@@ -0,0 +1,14 @@
+module sr_latch(
+  input s,r,
+  output reg q);
+  
+  always@(*)
+    begin
+      if(s&r)
+      q=1'bx;
+      else if(s)
+        q=1;
+      else if(r)
+        q=0;
+    end
+endmodule
