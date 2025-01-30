0398910f58d4b0e04dcbb6d12d7732d2d5076e3d
Sriniketh Sekar <148874325+SrinikethSekar@users.noreply.github.com>
2025-01-30 20:50:49 +0530

    Create Mealy_sequence_detector_tb.v

---
+++ Verilog/Day 7/Mealy Sequence Detector/Mealy_sequence_detector_tb.v
@@ -0,0 +1,39 @@
+module TB;
+  reg clk, rst, in;
+  wire out;
+  
+  fsm_seq sd(clk, rst, in, out);
+  initial clk = 0;   
+  always #2 clk = ~clk;
+    
+  initial begin
+    $monitor("clk=%b rst=%b in=%b out=%b",clk,rst,in,out);
+    in = 0;
+    #1 rst = 0;
+    #2 rst = 1;
+    
+    #3 in = 1;
+    #4 in = 1;
+    #4 in = 0;
+    #4 in = 1;
+    #4 in = 0;
+    #4 in = 1;
+    #4 in = 0;
+    #4 in = 1;
+    #4 in = 1;
+    #4 in = 1;
+    #4 in = 0;
+    #4 in = 1;
+    #4 in = 0;
+    #4 in = 1;
+    #4 in = 0;
+    #10;
+    $finish;
+  end
+  
+  initial begin
+    // Dump waves
+    $dumpfile("dump.vcd");
+    $dumpvars(0);
+  end
+endmodule
