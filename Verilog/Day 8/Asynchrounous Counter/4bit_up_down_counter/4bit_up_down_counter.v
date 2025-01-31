c8638077edaf0e5415d83a326baa7424ec1e7686
Sriniketh Sekar <148874325+SrinikethSekar@users.noreply.github.com>
2025-01-31 12:28:02 +0530

    Update 4bit_up_down_counter.v

--- Verilog/Day 8/Asynchrounous Counter/4bit_up_down_counter/4bit_up_down_counter.v
+++ Verilog/Day 8/Asynchrounous Counter/4bit_up_down_counter/4bit_up_down_counter.v
@@ -1,1 +1,15 @@
-a.
+module fourbit_up_down_counterA(
+  input rst,clk,
+  input up_down, //mode signal 1 means up ,2 means down
+  output reg [3:0]out);
+  
+  always@(posedge clk or posedge rst)
+   begin
+    if(rst)
+      out<=4'b0000;
+     else if(up_down)
+       out<=out+1;
+  else
+     out<=out-1;
+   end
+endmodule
