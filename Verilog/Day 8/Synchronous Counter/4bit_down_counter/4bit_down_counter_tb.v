068f3c8b68dbac751116f1c68fdcb5f376303193
Sriniketh Sekar <148874325+SrinikethSekar@users.noreply.github.com>
2025-01-31 12:07:20 +0530

    Create 4bit_down_counter_tb.v

---
+++ Verilog/Day 8/Synchronous Counter/4bit_down_counter/4bit_down_counter_tb.v
@@ -0,0 +1,28 @@
+module fourbit_down_counterS_tb();
+  reg rst,clk;
+  wire out;
+  
+  fourbit_down_counterS uut(rst,clk,out);
+  initial 
+    begin
+      clk=0;
+      forever #5 clk=~clk;
+  
+    end
+  initial begin
+    $dumpfile("waves.vcd");
+    $dumpvars;
+  end
+  
+  initial
+    begin
+      $monitor(" rst=%b clk=%b  out=%d",rst,clk,out);
+      
+      rst=1; #20;
+      rst=0; #100;
+      rst=1; #20;
+      rst=0; #100;
+      
+      $finish;
+    end
+endmodule
