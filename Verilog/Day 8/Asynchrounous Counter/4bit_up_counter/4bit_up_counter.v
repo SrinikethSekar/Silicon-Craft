81c47ee2e6c6abf89671c8a6105da6a7d757682c
Sriniketh Sekar <148874325+SrinikethSekar@users.noreply.github.com>
2025-01-31 11:53:37 +0530

    Create 4bit_up_counter.v

---
+++ Verilog/Day 8/Asynchrounous Counter/4bit_up_counter/4bit_up_counter.v
@@ -0,0 +1,12 @@
+module fourbit_up_counterA(
+  input rst,clk,
+  output reg [3:0]out);
+  
+  always@(posedge clk or posedge rst)
+   begin
+    if(rst)
+      out<=4'b0000;
+  else
+     out<=out+1;
+   end
+endmodule
